6155c6d27cf5c37cb9debd19ad8b6a12023ef9ed
Luis Checa <c008101c@student.staffs.ac.uk>
2014-11-05 14:11:07 +0000

    [+] Add HighLevel.UI framework [LC]

--- StealthOfTomorrow/AppMain.cs
+++ StealthOfTomorrow/AppMain.cs
@@ -6,12 +6,12 @@ using Sce.PlayStation.Core.Environment;
 using Sce.PlayStation.Core.Graphics;
 using Sce.PlayStation.Core.Input;
 using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.UI;
 
 namespace StealthOfTomorrow
 {
 	public class AppMain
 	{
-		private static GraphicsContext graphics;
 		
 		public static void Main (string[] args)
 		{
@@ -22,12 +22,14 @@ namespace StealthOfTomorrow
 				SystemEvents.CheckEvents ();	// We check system events (such as pressing PS button, pressing power button to sleep, major and unknown crash!!)
 				
 				Director.Instance.Update();
+				UISystem.Update(Touch.GetData(0)); // Update UI Manager
 				Director.Instance.Render();
-				
+			    UISystem.Render(); // Render UI Manager
 				Director.Instance.GL.Context.SwapBuffers(); // Swap between back and front buffer
 				Director.Instance.PostSwap(); // Must be called after swap buffers - not 100% sure, imagine it resets back buffer to black/white, unallocates tied resources for next swap
 			}
 			Director.Terminate();	// Kill (terminate) the director, hence ending 2D scene program, once we are done with the scene (clicking red X button)
+			Sce.PlayStation.HighLevel.UI.UISystem.Terminate();
 		}
 		
 		
@@ -35,6 +37,8 @@ namespace StealthOfTomorrow
 		{
 			Director.Initialize(); // Initialises the GameEngine2D supplied by Sony. This will ALWAYS be done if you plan on using the 2D Director
 			
+			UISystem.Initialize(Director.Instance.GL.Context);
+			UISystem.SetScene(new Sce.PlayStation.HighLevel.UI.Scene(), null);
 			DebugOverlay scnDebug = new DebugOverlay();	// Create our new Debug Scene			
 			
 			Director.Instance.RunWithScene(scnDebug, true);	// Tell the Director to run the scene
