d6183b41b78500c0780591f275bf3247939723a7
Luis Checa <c008101c@student.staffs.ac.uk>
2014-11-12 15:57:58 +0000

    [+] Moved HUD frame to class, bars update [LC]

--- StealthOfTomorrow/GameHUD.cs
+++ StealthOfTomorrow/GameHUD.cs
@@ -5,18 +5,17 @@ namespace StealthOfTomorrow
 {
 	public class GameHUD: Scene
 	{
-		private ImageAsset thumbImage, healthImage, energyImage, livesImage, frameImage;
 		private int screenWidth, screenHeight;
+		private PlayerHUD playerHUD;
 		private float Y_PADDING = 5f;
-		private float MAX_LIVES = 4f;
-		private float MAX_VALUE = 100f;
 		
 		public GameHUD (Character player): base ()
 		{
 			float xFactor = 0.5f;
 			
 			Initialise();
-			this.RootWidget.AddChildLast(PlayerHUD(player, screenWidth * xFactor, Y_PADDING));
+			playerHUD = new PlayerHUD(player, screenWidth * xFactor, Y_PADDING);
+			this.RootWidget.AddChildLast(playerHUD);
 		}
 		
 		public GameHUD(Character[] players, int maxPlayers): base()
@@ -28,7 +27,7 @@ namespace StealthOfTomorrow
 			
 			for(int i=1; i <= playerCount; i++)
 			{
-				this.RootWidget.AddChildLast(PlayerHUD(players[i], screenWidth * xFactor * i, Y_PADDING));
+				this.RootWidget.AddChildLast(new PlayerHUD(players[i], screenWidth * xFactor * i, Y_PADDING));
 			}
 		}
 		
@@ -42,84 +41,128 @@ namespace StealthOfTomorrow
 			screenWidth = UISystem.FramebufferWidth;
 			screenHeight = UISystem.FramebufferHeight;
 			
-			// Load images (reused)
-			thumbImage = new ImageAsset("/Application/Assets/bird1.png");
-			frameImage = new ImageAsset("/Application/Assets/HealthGUI/EmptyBar.png");
-			energyImage = new ImageAsset("/Application/Assets/HealthGUI/StaminaBar.png");
-			healthImage = new ImageAsset("/Application/Assets/HealthGUI/HealthBar.png");
-			livesImage = new ImageAsset("/Application/Assets/HealthGUI/Lives.png");
+		}
+		override protected void OnUpdate(float dt)
+		{
+			playerHUD.Update(dt);
 		}
 		
-		private Panel PlayerHUD(Character player, float x, float y)
+	}
+	
+	class PlayerHUD : Panel
+	{
+		// Load images (reused)
+		private static ImageAsset thumbImage = new ImageAsset("/Application/Assets/bird1.png");
+		private static ImageAsset frameImage = new ImageAsset("/Application/Assets/HealthGUI/EmptyBar.png");
+		private static ImageAsset verticalFrameImage = new ImageAsset("/Application/Assets/HealthGUI/EmptyVerticalBar.png");
+		private static ImageAsset energyImage = new ImageAsset("/Application/Assets/HealthGUI/StaminaBar.png");
+		private static ImageAsset healthImage = new ImageAsset("/Application/Assets/HealthGUI/HealthBar.png");
+		private static ImageAsset livesImage = new ImageAsset("/Application/Assets/HealthGUI/Lives.png");
+		
+		private const int MAX_LIVES = 4;
+		private const float MAX_VALUE = 100f;
+		
+		private Character player;
+		private ImageBox energyBar, healthBar;
+		private ImageBox[] lives;
+		
+		private float verticalFrameHeight, horizontalFrameWidth;
+		
+		public PlayerHUD(Character player, float x, float y): base()
 		{
-			Panel panel = new Panel();
+			this.player = player;
 			float xPos = 0f;
 			float yPos = 0f;
 			
 			// Thumbnail
 			ImageBox thumb = new ImageBox();
 			thumb.Image = thumbImage;
-			thumb.ImageScaleType = ImageScaleType.Stretch;
+			thumb.ImageScaleType = ImageScaleType.AspectInside;
 			thumb.SetSize(thumbImage.Width, thumbImage.Height);
 			yPos += thumb.Height;
 			xPos -= thumb.Width/2;
 			thumb.SetPosition(xPos, yPos);
-			panel.AddChildLast(thumb);
-			
-			float barHeight = thumb.Height - 10f;
-			// Energy bar frame
+			this.AddChildLast(thumb);
+			// Move cursor to right end of thumbnail
 			xPos += thumb.Width;
-			ImageBox energyFrame = new ImageBox();
-			energyFrame.Image = frameImage;
-			energyFrame.ImageScaleType = ImageScaleType.Stretch;
-			energyFrame.SetPosition(xPos, yPos);
-			energyFrame.SetSize(frameImage.Width, barHeight);
-			panel.AddChildLast(energyFrame);
-			// Energy bar
-			ImageBox energy = new ImageBox();
-			energy.Image = energyImage;
-			energy.ImageScaleType = ImageScaleType.Stretch;
-			energy.SetPosition(xPos, yPos);
-			energy.SetSize(energyImage.Width, barHeight * player.Energy / MAX_VALUE);
-			panel.AddChildLast(energy);
+			
+			// Frame dimensions
+			verticalFrameHeight = thumb.Height;
+			float verticalFrameWidth = (verticalFrameHeight / verticalFrameImage.Height) * verticalFrameImage.Width;
+			
 			// Lives bar frame
-			xPos += energy.Width;
+			float livesSize = verticalFrameHeight / MAX_LIVES;
 			ImageBox livesFrame = new ImageBox();
-			livesFrame.Image = frameImage;
+			livesFrame.Image = verticalFrameImage;
 			livesFrame.ImageScaleType = ImageScaleType.Stretch;
 			livesFrame.SetPosition(xPos, yPos);
-			livesFrame.SetSize(frameImage.Width, barHeight);
-			panel.AddChildLast(livesFrame);
-			// Lives bar
-			ImageBox lives = new ImageBox();
-			lives.Image = livesImage;
-			lives.ImageScaleType = ImageScaleType.Stretch;
-			lives.SetPosition(xPos, yPos);
-			lives.SetSize(livesImage.Width, barHeight * player.Lives / MAX_LIVES);
-			panel.AddChildLast(lives);
+			livesFrame.SetSize(livesSize, verticalFrameHeight);
+			this.AddChildLast(livesFrame);
+			// Lives in bar
+			lives = new ImageBox[MAX_LIVES];
+			for (int i = 0; i < player.Lives; i++)
+			{
+				lives[i] = new ImageBox();
+				lives[i].Image = livesImage;
+				lives[i].ImageScaleType = ImageScaleType.AspectInside;
+				lives[i].SetPosition(xPos, yPos);
+				lives[i].SetSize(livesSize, livesSize);
+				this.AddChildLast(lives[i]);
+				yPos += livesSize;
+			}
+			// Move cursor to top right end of frame
+			xPos += livesSize;
+			yPos -= livesSize * player.Lives;
 			
-			float healthBarMaxWidth = thumb.Width + energyFrame.Width + livesFrame.Width;
+			// Energy bar frame
+			ImageBox energyFrame = new ImageBox();
+			energyFrame.Image = verticalFrameImage;
+			energyFrame.ImageScaleType = ImageScaleType.Stretch;
+			energyFrame.SetPosition(xPos, yPos);
+			energyFrame.SetSize(verticalFrameWidth, verticalFrameHeight);
+			this.AddChildLast(energyFrame);
+			// Energy bar
+			energyBar = new ImageBox();
+			energyBar.Image = energyImage;
+			energyBar.ImageScaleType = ImageScaleType.Stretch;
+			energyBar.SetPosition(xPos, yPos);
+			energyBar.SetSize(verticalFrameWidth, verticalFrameHeight); // * player.Energy / MAX_VALUE);
+			this.AddChildLast(energyBar);
+			
+			// Horizontal frame dimensions
+			horizontalFrameWidth = thumb.Width + livesSize + verticalFrameWidth;
+			float horizontalFrameHeight = (healthImage.Width / horizontalFrameWidth) * healthImage.Height;
+			
+			// Move down the frame height, and left the thumbnail and lives frame width
+			yPos -= horizontalFrameHeight;
+			xPos -= thumb.Width + livesSize;
 			// Health bar frame
-			xPos -= thumb.Width + energy.Width;
 			ImageBox healthFrame = new ImageBox();
 			healthFrame.Image = frameImage;
 			healthFrame.ImageScaleType = ImageScaleType.Stretch;
-			healthFrame.SetSize(healthBarMaxWidth, frameImage.Height);
-			// Health bar
-			ImageBox health = new ImageBox();
-			health.Image = healthImage;
-			health.ImageScaleType = ImageScaleType.Stretch;
-			health.SetSize(healthBarMaxWidth * player.Health/MAX_VALUE, healthImage.Height);
-			// Move down the bar height, and left the thumbnail and energy bar width
-			yPos -= health.Height;
+			healthFrame.SetSize(horizontalFrameWidth, horizontalFrameHeight);
 			healthFrame.SetPosition(xPos, yPos);
-			health.SetPosition(xPos, yPos);
-			panel.AddChildLast(healthFrame);
-			panel.AddChildLast(health);
+			this.AddChildLast(healthFrame);
+			// Health bar
+			healthBar = new ImageBox();
+			healthBar.Image = healthImage;
+			healthBar.ImageScaleType = ImageScaleType.Stretch;
+			healthBar.SetSize(horizontalFrameWidth, horizontalFrameHeight);
+			healthBar.SetPosition(xPos, yPos);
+			this.AddChildLast(healthBar);
 			
-			// Position panel
-			panel.SetPosition(x, y);
-			return panel;
+			// Position this
+			this.SetPosition(x, y);
 		}
-	}
+		
+		public void Update(float dt)
+		{
+			energyBar.Height = verticalFrameHeight * player.Energy / MAX_VALUE;
+			healthBar.Width = horizontalFrameWidth * player.Health/MAX_VALUE;
+			for(int i = MAX_LIVES - 1; i >= player.Lives; i--)
+			{
+				lives[i].Visible = false;
+			}
+		}
+}
 }
